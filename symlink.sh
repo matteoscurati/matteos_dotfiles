e09b91b55d1e7d1ae7fbc8ec2933a5ef18b61a20
Matteo Scurati <matteo.scurati@gmail.com>
2014-10-19 12:54:03 +0200

    Symlink script added

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Creare sym link
+
+ln -s /Users/matteoscurati/matteos_dotfiles/gitconfig.local /Users/matteoscurati/.gitconfig.local
+ln -s /Users/matteoscurati/matteos_dotfiles/vimrc.bundles.local /Users/matteoscurati/.vimrc.bundles.local
+ln -s /Users/matteoscurati/matteos_dotfiles/vimrc.local /Users/matteoscurati/.vimrc.local
+ln -s /Users/matteoscurati/matteos_dotfiles/zshrc.local /Users/matteoscurati/.zshrc.local
