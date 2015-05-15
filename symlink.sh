495c74560282cc4407a6a12c85867e5d52be30bc
Matteo Scurati <matteo.scurati@gmail.com>
2015-05-15 15:23:40 +0200

    better link

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Creare sym link
 
-ln -s /Users/matteoscurati/matteos_dotfiles/gitconfig.local /Users/matteoscurati/.gitconfig.local
-ln -s /Users/matteoscurati/matteos_dotfiles/vimrc.bundles.local /Users/matteoscurati/.vimrc.bundles.local
-ln -s /Users/matteoscurati/matteos_dotfiles/vimrc.local /Users/matteoscurati/.vimrc.local
-ln -s /Users/matteoscurati/matteos_dotfiles/zshrc.local /Users/matteoscurati/.zshrc.local
-ln -s /Users/matteoscurati/matteoscurati/gitexcludes /Users/matteoscurati/.gitexcludes
-ln -s /Users/matteoscurati/matteos_dotfiles/tumx.conf.local /Users/matteoscurati/.tumx.conf.local
+ln -s ~/matteos_dotfiles/gitconfig.local ~/.gitconfig.local
+ln -s ~/matteos_dotfiles/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/matteos_dotfiles/vimrc.local ~/.vimrc.local
+ln -s ~/matteos_dotfiles/zshrc.local ~/.zshrc.local
+ln -s ~/matteoscurati/gitexcludes ~/.gitexcludes
+ln -s ~/matteos_dotfiles/tumx.conf.local ~/.tumx.conf.local
