637c280cd2de5982ee2da46d54d455efd0165c31
Matteo Scurati <matteo.scurati@gmail.com>
2015-10-18 15:04:19 +0200

    Better plugin configuration

--- symlink.sh
+++ symlink.sh
@@ -2,8 +2,8 @@
 # Creare sym link
 
 ln -s ~/matteos_dotfiles/gitconfig.local ~/.gitconfig.local
-ln -s ~/matteos_dotfiles/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/matteos_dotfiles/vimrc.local ~/.vimrc.local
+ln -s ~/matteos_dotfiles/vim/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/matteos_dotfiles/vim/vimrc.local ~/.vimrc.local
 ln -s ~/matteos_dotfiles/zshrc.local ~/.zshrc.local
 ln -s ~/matteoscurati/gitexcludes ~/.gitexcludes
 ln -s ~/matteos_dotfiles/tumx.conf.local ~/.tumx.conf.local
